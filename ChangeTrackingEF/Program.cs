744e450bbffbc0ee8e18db22413c7fc4766a359f
Swagat Kumar Swain <ssswagatss@gmail.com>
2018-08-31 08:01:48 +0530

    Add project files.

---
+++ ChangeTrackingEF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChangeTrackingEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
